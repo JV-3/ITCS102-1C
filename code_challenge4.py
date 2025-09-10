5ea3e9d992bf1c62ec22ad11d9f968eab96a35d7
JV-3 <balanejohnvincent@gmail.com>
2025-09-10 23:07:11 +0800

    Update code_challenge4.py

--- code_challenge4.py
+++ code_challenge4.py
@@ -1,24 +1,22 @@
-print ("Good day, Welcome to the Manga Recommendation Center")
-x = input ("Are you interested ? yes/no ") 
+print("Good day, Welcome to the Manga Recommendation Center")
+x = input("Are you interested? yes/no ")
 
-if ( x == "yes" ) or ( x == "yes" ) or ( x == "yes" ) :
-	anime = input ("What genre do you seem to be interested with : comedy/action/horror \n ") .lower() == "yes"
-	decade = input ("What decades range of manga do you want to read : 1 (1990s) , 2 (2000s) \n ") .lower() == "yes"
-	L = input ("How long do you want to read a kind of manga: 1 ( long ) , 2 (short) \n ") .lower() == "yes"
-else:
-	print("Okay thank you, come back if you're interested in reading some manga again")
+if x.lower() == "yes":
+    anime = input("What genre do you seem to be interested with: comedy/action/horror \n").lower()
+    decade = input("What decades range of manga do you want to read: 1 (1990s) , 2 (2000s) \n").lower()
+    L = input("How long do you want to read a kind of manga: 1 (long) , 2 (short) \n").lower()
 
-if (anime == "comedy") and (decade == '1') and (L == '1') :
-	print ("I would highly recommend this to you (Great Teacher Onizuka) ")
-elif (anime == "comedy") and (decade == '2') and (L == '2') :
-	print ("I would highly recommend this to you (Yakitate Japan) ")
-elif (anime == "action") and (decade == '1') and (L == '1') :
-	print ("I would highly recommend this to you (Hunter X Hunter) ")
-elif (anime == "action") and (decade == '2') and (L == '2') : 
-	print ("I would highly recommend this to you (Death Note) ")
-elif (anime == "horror") and (decade == '1') and (L == '1') :
-	print ("I would highly recommend this to you (Berserk) ")
-elif (anime == "horror") and (decade == '2') and (L == '2') :
-	print ("I would highly recommend this to you (Tokyo Ghoul) ")
-else :
-	print ("Sorry, I don't have other type of genre that I could recommend to you, SORRY! ")
+    if anime == "comedy" and decade == '1' and L == '1':
+        print("I would highly recommend this to you (Great Teacher Onizuka)")
+    elif anime == "comedy" and decade == '2' and L == '2':
+        print("I would highly recommend this to you (Yakitate Japan)")
+    elif anime == "action" and decade == '1' and L == '1':
+        print("I would highly recommend this to you (Hunter X Hunter)")
+    elif anime == "action" and decade == '2' and L == '2':
+        print("I would highly recommend this to you (Death Note)")
+    elif anime == "horror" and decade == '1' and L == '1':
+        print("I would highly recommend this to you (Berserk)")
+    elif anime == "horror" and decade == '2' and L == '2':
+        print("I would highly recommend this to you (Tokyo Ghoul)")
+    else:
+        print("Sorry, I don't have other type of genre that I could recommend to you, SORRY!")
